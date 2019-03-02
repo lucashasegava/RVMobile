ac2a4fe03aff1c06f33bb4acc2c2dd9c1d95dfab
Lucas Hasegava <lucas.hasegava@gmail.com>
2019-03-01 23:32:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rvmobile/CarBuilder/CarBuilderActivity.kt
@@ -0,0 +1,10 @@
+package com.example.rvmobile.CarBuilder
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class CarBuilderActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
